fea0845ab7b6a05a87b9697bbc40552172938c1d
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-04-12 20:12:25 +0200

    started design components

--- src/components/AllPokemon.js
+++ src/components/AllPokemon.js
@@ -1,100 +1,165 @@
-import React, { useState, useEffect} from 'react'
-import { getPokemons } from '../services/pokemons'
-import PokemonCard from './PokemonCard'
-import SearchBar from './SearchBar';
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import { toFirstCharUppercase} from "../constants"
+import { toFirstCharUppercase } from "../constants"
 import { CircularProgress } from '@material-ui/core'
+import styled from 'styled-components';
+
 
 const AllPokemon = (props) => {
-    const [pokemons, setPokemons] = useState([])
+  const { history } = props;
+  const [pokemonData, setPokemonData] = useState({})
+  const [filter, setFilter] = useState("");
 
-    // useEffect(() => {
-    //     getPokemons().then((data) => {
-    //         setPokemons(data);
-    //     })
-    // }, [])
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          // console.log(pokemon)
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+        // console.log(newPokemonData)
+      });
+  }, []);
 
-       // console.log(pokemons)
-    // const [pokemons, setPokemons] = useState([])
-    const { history } = props;
-    const [pokemonData, setPokemonData] = useState({})
-    const [filter, setFilter] = useState("");
-    
-    useEffect(() => {
-        axios
-          .get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-          .then(function (response) {
-            const { data } = response;
-            const { results } = data;
-            const newPokemonData = {};
-            results.forEach((pokemon, index) => {
-                // console.log(pokemon)
-              newPokemonData[index + 1] = {
-                id: index + 1,
-                name: pokemon.name,
-                sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                    index + 1}.png`,
-              };
-            });
-            setPokemonData(newPokemonData);
-            // console.log(newPokemonData)
-          });
-      }, []);
+  const inputHandler = (event) => {
+    event.preventDefault()
 
-    const inputHandler = (event) => {
-        event.preventDefault()
-        
-        // console.log("inputloooooower",event.target.value)
-        setFilter(event.target.value);
+    // console.log("inputloooooower",event.target.value)
+    setFilter(event.target.value);
 
-    }
+  }
 
-    const getPokemonCard = (pokemonId) => {
-        const { id, name, sprite } = pokemonData[pokemonId];
-        return (
-          <div  key={pokemonId}>
-            {console.log(id)}
-            <div onClick={() => history.push(`/${id}`)}>
-              <div>
-              <img src={sprite} style={{ width: "130px", height: "130px" }}/>
-              </div> 
-              <div>
-                <div>{`${id}. ${toFirstCharUppercase(name)}`}</div>
-              </div>
-            </div>
-          </div>
-        );
-      };
+  const getPokemonCard = (pokemonId) => {
+    const { id, name, sprite } = pokemonData[pokemonId];
+    return (
+      <WrapPokemon key={pokemonId}>
+        {/* {console.log(id)} */}
+        <div onClick={() => history.push(`/${id}`)}>
+          <ContainerCardPokemon>
+            <img src={sprite} style={{ width: "150px", height: "150px" }} />
+            <br/>
+            <p >{`${id}. ${toFirstCharUppercase(name)}`}</p>
+          </ContainerCardPokemon>
+        </div>
+      </WrapPokemon>
+    );
+  };
 
-    
+
   return (
-    <div>
-        <input
-        type = "search"
+    <ContainerPokemon>
+      <input
+        type="search"
         onChange={inputHandler}
-        value= {filter}
+        value={filter}
         placeholder='Search...'
+      />
+      <div>
+        {/* {pokemonData ? ( */}
+          <ContentCardPokemon  >
+            {Object.keys(pokemonData).map(
+              (pokemonId) =>
+                pokemonData[pokemonId].name.includes(filter) &&
+                getPokemonCard(pokemonId)
+
+            )}
+          </ContentCardPokemon>
+        {/* ) : (
+          <CircularProgress /> */}
         
-        />
-        <div>
-        {pokemonData ? (
-        <div  >
-          {Object.keys(pokemonData).map(
-            (pokemonId) =>
-              pokemonData[pokemonId].name.includes(filter) &&
-              getPokemonCard(pokemonId)
-              
-          )}
-        </div>
-      ) : (
-        <CircularProgress/>
-      )}
 
-        </div>
+      </div>
 
-    </div>
+    </ContainerPokemon>
   )
 }
 
 export default AllPokemon
+
+
+const ContainerPokemon = styled.div`
+// background-color: white;
+color: black;
+padding: 0 0 26px;
+margin: 30px 50px;
+
+
+input {
+  width: 30%;
+  padding: 10px;
+  margin-bottom: 50px;
+  border-radius: 20px;
+  // outline: none;
+}
+`
+
+const ContainerCardPokemon = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+justify-content: center;
+`
+
+const ContentCardPokemon = styled.div`
+display: grid;
+grid-gap: 25px;
+gap: 30px;
+grid-template-columns: repeat(6, minmax(0, 1fr));
+
+
+@media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+}
+@media (max-width: 480px) {
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+}
+`;
+
+const WrapPokemon = styled.div`
+padding-top: 56.25;
+border-radius: 10px;
+box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+cursor: pointer;
+overflow: hidden;
+position:relative;
+transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+border: 3px solid rgba(249, 249, 249, 0.1);
+background-color: white;
+&:hover {
+  box-shadow: -2px 1px 24px 8px rgba(219,180,22,0.79);
+  -webkit-box-shadow: -2px 1px 24px 8px rgba(219,180,22,0.79);
+  -moz-box-shadow: -2px 1px 24px 8px rgba(219,180,22,0.79);
+    // box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+    //   rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+}
+img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: relative;
+    transition: opacity 500ms ease-in-out 0s;
+    z-index: 1;
+    top:  0;    
+    padding: 5px;
+
+}
+
+
+`
+
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,91 +1,156 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import {  Link, CircularProgress, Button } from "@mui/material";
+import { Link, CircularProgress } from "@mui/material";
 import { toFirstCharUppercase } from '../constants';
+import styled from 'styled-components';
 
 const PokemonCard = (props) => {
-  console.log("prrrrooops",props)
+  console.log("prrrrooops", props)
 
-  const {match, history} = props;
-  const {params} = match;
-  const {pokemonId} = params;
+  const { match, history } = props;
+  const { params } = match;
+  const { pokemonId } = params;
   const [pokemon, setPokemon] = useState(undefined);
 
 
   useEffect(() => {
     axios
-    .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
-    .then(function(response) {
-      console.log(response)
-      const {data} = response;
-      setPokemon(data);
-    })
-    .catch(function(error) {
-      setPokemon(false);
-    })
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+      .then(function (response) {
+        console.log(response)
+        const { data } = response;
+        setPokemon(data);
+      })
+      .catch(function (error) {
+        setPokemon(false);
+      })
   }, [pokemonId]);
 
 
   const generatePokemon = (pokemon) => {
     console.log(pokemon)
-    const {name, id, species, height, weight, types, sprites} = pokemon;
-    // const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
-    const {front_default} = sprites;
+    const { name, id, species, height, weight, types, sprites } = pokemon;
+    const { front_default } = sprites;
 
     return (
-      <>
-      <div>
-        {`${id}.`} {toFirstCharUppercase(name)}
-            <img src={front_default} alt="" />
-      </div>
-  
-      <div>
-      <img style={{ width: "150px", height: "150px" }} src={front_default} alt=""/>
-      <p>Pokemon info</p>
-      <div>
-        {"Species:"}
-        <Link href={species.url}>{species.name}</Link>
-      </div>
-      
-      <div>
-        <p>Height : {height}</p>
-        <p>Weight : {weight}</p>
-      </div>
-  
-      <div>
-        <p>Types:</p>
-        {types.map((typeInfo) => {
-          console.log(typeInfo)
-          const {type} = typeInfo;
-          const {name} = type;
-          return (
-           <div key= {name}>{`${name}`}</div>
-           )
-        })}
-      </div>
-        </div>
-        </>
+      <ContainerTop>
+        <HeaderPokemonCard>
+          <img style={{ width: "150px", height: "150px" }} src={front_default} alt="" />
+        </HeaderPokemonCard>
+        
+
+        <TextPokemonCard>
+          {/* <img style={{ width: "150px", height: "150px" }} src={front_default} alt="" /> */}
+          <p>POKEMON INFO</p>
+          
+            {"Name: "}
+            <Link href={species.url}>{species.name}</Link>
+          
+
+          {/* <div> */}
+            <p>Height : {height}</p>
+            <p>Weight : {weight}</p>
+          {/* </div>
+
+          <div> */}
+            <p>Types:</p>
+            {types.map((typeInfo) => {
+              console.log(typeInfo)
+              const { type } = typeInfo;
+              const { name } = type;
+              return (
+                <div key={name}>{`${name}`}</div>
+              )
+            })}
+          {/* </div> */}
+        </TextPokemonCard>
+      </ContainerTop>
     )
   }
   
 
   return (
-    <>
-      {pokemon === undefined && <CircularProgress />}
+    <ContentPokemonCard>
+      {/* {pokemon === undefined && <CircularProgress />} */}
       {pokemon !== undefined && pokemon && generatePokemon(pokemon)}
       {pokemon === false && <p> Pokemon not found</p>}
 
       {pokemon !== undefined && (
-        <Button variant="contained" onClick={() => history.push("/")}>
+        <Button onClick={() => history.push("/")}>
           back to pokedex
         </Button>
       )}
-    </>
+    </ContentPokemonCard>
   );
 };
 
-  
+const ContainerTop = styled.div`
+background-color: white;
+width: 50%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+border-radius: 10px;
+`
+
+const ContentPokemonCard = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+margin-top: 20%;
+
+ 
+`
+
+const HeaderPokemonCard = styled.div`
+position: relative;
+height: 300px;
+
+background-image: linear-gradient(315deg, #000000 0%, #414141 74%);
+border-bottom-left-radius: 50% 20%;
+border-bottom-right-radius: 50% 20%;
+width: 100%;
+height: 20%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+`
+
+const TextPokemonCard = styled.div`
+width: 50%;
+background-color: white;
+color: black;
+position: relative;
+padding: 20px;
+
+`
+
+const Button = styled.button`
+margin: 20px auto;
+background-color: yellow;
+font-size: 15px;
+padding: 0px 24px;
+height: 56px;
+border-radius: 10px;
+display:flex;
+align-items: center;
+cursor: ponter;
+justify-content: center;
+letter-spacing: 1.8px;
+text-align: center;
+text-transform: uppercase;
+// background: rgb(249, 249, 249);
+border: none;
+// color: rgb(0, 0, 0)
+&:hover {
+  background: rgba(219,180,22,0.79)
 
 
-
+`
 export default PokemonCard
